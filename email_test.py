1021772e0a8c307d11614fd2093e5d731da81fe2
Stuart Hoye <stuarthoye@gmail.com>
2017-06-04 22:36:52 -0600

    More rough-in

--- email_test.py
+++ email_test.py
@@ -7,17 +7,21 @@ import smtplib
 # Import the email modules we'll need
 from email.mime.text import MIMEText
 
-msg = MIMEText("HELLO WORLD!")
+def send_notification(team, lab, url):
+    msg = "This is a notification:/n"
+    msg += "Your team, {}, has been assigned a repo for lab {}.".format(team, lab)
+    msg += "The url for your repo is: {}".format(url)
+    msg = MIMEText(msg)
 
-# me == the sender's email address
-# you == the recipient's email address
-msg['Subject'] = 'The contents of %s' % msg
-msg['From'] = "hoye@ualberta.ca"
-msg['To'] = "hoye@ualberta.ca"
+    # me == the sender's email address
+    # you == the recipient's email address
+    msg['Subject'] = 'The contents of %s' % msg
+    msg['From'] = "hoye@ualberta.ca"
+    msg['To'] = "hoye@ualberta.ca"
 
-# Send the message via our own SMTP server.
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login("hoye@gmail.com", "H0YE57Uar7")
-s.send_message(msg)
-s.quit()
+    # Send the message via our own SMTP server.
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(uname, pword)        # TODO: READ THESE FROM FILE
+    s.send_message(msg)
+    s.quit()
