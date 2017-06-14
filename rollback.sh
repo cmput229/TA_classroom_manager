e965d034b8761b95e301176e990c039b4a928066
stuarthoye <stuarthoye@gmail.com>
2017-06-14 11:01:08 -0600

    Suppress some rollback output.

--- rollback.sh
+++ rollback.sh
@@ -2,13 +2,15 @@
 
 # https://stackoverflow.com/questions/4114095/how-to-revert-git-repository-to-a-previous-commit
 # https://www.git-tower.com/learn/git/faq/restore-repo-to-previous-revision
+# https://stackoverflow.com/questions/8943693/can-git-operate-in-silent-mode
+# http://www.tldp.org/LDP/abs/html/io-redirection.html
 
 DIR="$1"
 COMMIT="$2"
 
 cd $DIR
-git checkout master                         # Ensure we're on the master
-git branch -d deadline                      # Ensure that there isn't already a deadline branch
-git checkout -b deadline $COMMIT            # Create & checkout a new branch called deadline at or before the deadline
-git push --set-upstream origin deadline     # Creates a remote upstream branch; lets students see what's being marked.
+git checkout master -q                     # Ensure we're on the master
+git branch -d deadline -q                  # Ensure that there isn't already a deadline branch
+git checkout -b deadline $COMMIT -q        # Create & checkout a new branch called deadline at or before the deadline
+git push --set-upstream origin deadline -q # Creates a remote upstream branch; lets students see what's being marked.
 
