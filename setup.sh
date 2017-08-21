fbff7e857215243a85285a683b4d1e83766de4a9
stuarthoye <stuarthoye@gmail.com>
2017-08-21 10:13:33 -0600

    Add gmail configuration to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,8 @@
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
+cd ./gmail
+pip install --upgrade google-api-python-client
+cd ../
+python ./gmail/quickstart.py
 deactivate
